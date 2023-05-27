66c820e8a39418648fc907832b8d0e0ce202138b
sannyspass <sannyspass@gmail.com>
2023-05-27 15:31:19 +0900

    Solved

---
+++ 01-Basic-Syntax-Conditional-Statements-Loops/Lab-Exercises/Excellent-Grade.js
@@ -0,0 +1,16 @@
+/* 2. Excellent Grade
+
+Write a function that receives a single number and checks if the grade is excellent or not. 
+If it is, print "Excellent", otherwise print "Not excellent". */
+
+function solve(grade) {
+    if (grade >= 5) {
+        console.log("Excellent!");
+    } else if (grade >= 2) {
+        console.log("Bad.");
+    } else if (grade <= 2) {
+        console.log("Fail.")
+    }
+}
+
+solve(1);
---
+++ 01-Basic-Syntax-Conditional-Statements-Loops/Lab-Exercises/Multiply-by-2.js
@@ -0,0 +1,9 @@
+/* 1. Multiply Number by 2
+
+Write a function that receives a number and prints as result that number multiplied by two. */
+
+function solve(num) {
+    console.log(num * 2);
+}
+
+solve(2);
