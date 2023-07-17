4a2d1a8fd8329198cd5fe282f6e5c952504cf9d5
sannyspass <sannyspass@gmail.com>
2023-07-17 15:06:07 +0900

    Updated

---
+++ Angela-Yu-JavaScript-Exercises/Leap Year.js
@@ -0,0 +1,16 @@
+function isLeap(year) {
+    
+    //divide by 4
+    //divide by 100
+    //divide by 400
+    
+    if (year % 4 === 0) {
+        if (year % 100 ===0) {
+            
+        }
+
+    }
+
+}
+
+isLeap(2010);
