11ad8ba0b345e6ec070c252ae49e25eff5a9c4ef
Sanny <124334347+sannyspass@users.noreply.github.com>
2023-08-09 16:03:08 +0900

    New Problems
    Added new problems to be solved.

--- Soft-Uni-JS-Basics/01-Simple-Calculations-And-Operations/Extra-Exercises/USD to BGN.js
+++ Soft-Uni-JS-Basics/01-Simple-Calculations-And-Operations/Extra-Exercises/USD to BGN.js
@@ -3,10 +3,25 @@
 Напишете програма за конвертиране на щатски долари (USD) в български лева (BGN). 
 Закръглете резултата до 2 цифри след десетичната запетая. Използвайте фиксиран курс между долар и лев: 1 USD = 1.79549 BGN.*/
 
-function moneyConvertor(input) {
+/*function moneyConvertor(input) {
   let usd = Number(input.shift());
   let bgn = usd * 1.79549;
   console.log(`You have ${bgn.toFixed(2)}lv.`);
 }
 
-moneyConvertor([5]);
+moneyConvertor([5]);*/
+
+/*function ageLimit (age) {
+if (age >=14 && age <= 90) {
+  console.log("True.");
+} else {
+  console.log("False.");
+}
+}
+
+ageLimit(12);*/
+
+let age = 25;
+if (age > 14 && age <= 90) {
+  console.log("true");
+}
