7dc735e0b1aae0380931d581dc5dab781e079cad
Sanny <124334347+sannyspass@users.noreply.github.com>
2023-08-23 16:19:29 +0900

    Updated Problems

---
+++ Soft-Uni-JS-Programming-Basics/03-Conditional-Constructions/03-Lab-Exercises/Animal Species.js
@@ -0,0 +1,27 @@
+/*2. Клас животно
+Напишете програма, която отпечатва класа на животното според неговото име, въведено от потребителя.
+1. dog -> mammal
+2. crocodile, tortoise, snake -> reptile
+3. others -> unknown*/
+
+function animalSpecies(input) {
+  let animal = input.shift();
+
+  switch (animal) {
+    case "dog":
+      console.log("mammal");
+      break;
+
+    case "crocodile":
+    case "tortoise":
+    case "snake":
+      console.log("reptile");
+      break;
+
+    default:
+      console.log("unknown");
+      break;
+  }
+}
+
+animalSpecies(["rabbit"]);
