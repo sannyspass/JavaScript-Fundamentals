c4d9b0599324b7da7b2bf0615ca20fd89146b69d
sannyspass <sannyspass@gmail.com>
2023-08-24 13:55:39 +0900

    Update
    New problem solved.

--- Soft-Uni-JS-Programming-Basics/03-Conditional-Constructions/03-Lab-Exercises/Age and Gender.js
+++ Soft-Uni-JS-Programming-Basics/03-Conditional-Constructions/03-Lab-Exercises/Age and Gender.js
@@ -9,3 +9,26 @@
 · "Ms." – жена (пол 'f') на 16 или повече години
 
 · "Miss" – момиче (пол 'f') под 16 години*/
+
+function greeting(input) {
+  let age = Number(input.shift());
+  let gender = input.shift();
+
+  if (gender == "f") {
+    if (age >= 16) {
+      console.log("Mrs.");
+    } else {
+      console.log("Miss");
+    }
+  }
+
+  if (gender == "m") {
+    if (age >= 16) {
+      console.log("Mr.");
+    } else {
+      console.log("Master");
+    }
+  }
+}
+
+greeting([23, "m"]);
