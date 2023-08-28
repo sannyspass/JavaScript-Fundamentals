ad141d4fe7b891281ada2a8b4905cd6a4e6a76e1
sannyspass <sannyspass@gmail.com>
2023-08-28 13:19:12 +0900

    Solved Problem
    Update

--- Soft-Uni-JS-Programming-Basics/03-Conditional-Constructions/03-Lab-Exercises/Company Taxes.js
+++ Soft-Uni-JS-Programming-Basics/03-Conditional-Constructions/03-Lab-Exercises/Company Taxes.js
@@ -18,7 +18,49 @@ function comissions(input) {
   let sales = Number(input.shift());
   let comission = 0;
 
-  if (city === "Sofia" && 0 < sales < 500) {
-    comission = 0.05;
+  if (city === "Sofia") {
+    if (sales > 0 && sales <= 500) {
+      comission = 0.05;
+    } else if (sales > 500 && sales <= 1000) {
+      comission = 0.07;
+    } else if (sales > 1000 && sales <= 10000) {
+      comission = 0.08;
+    } else if (sales > 10000) {
+      comission = 0.12;
+    } else {
+      console.log("invalid");
+    }
   }
+
+  if (city === "Varna") {
+    if (sales > 0 && sales <= 500) {
+      comission = 0.045;
+    } else if (sales > 500 && sales <= 1000) {
+      comission = 0.075;
+    } else if (sales > 1000 && sales <= 10000) {
+      comission = 0.1;
+    } else if (sales > 10000) {
+      comission = 0.13;
+    } else {
+      console.log("invalid");
+    }
+  }
+
+  if (city === "Plovdiv") {
+    if (sales > 0 && sales <= 500) {
+      comission = 0.055;
+    } else if (sales > 500 && sales <= 1000) {
+      comission = 0.08;
+    } else if (sales > 1000 && sales <= 10000) {
+      comission = 0.12;
+    } else if (sales > 10000) {
+      comission = 0.145;
+    } else {
+      console.log("invalid");
+    }
+  }
+
+  console.log((sales * comission).toFixed(2));
 }
+
+comissions(["Plovdiv", 499.99]);
