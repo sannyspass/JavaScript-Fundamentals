aba9e03a40136aa8c533ce0a5dd8a543809c75d9
Sanny <124334347+sannyspass@users.noreply.github.com>
2023-09-21 18:40:36 +0900

    Update

--- Soft-Uni-JS-Programming-Basics/05-For-Loops/05-Lab-Exercises/Symbols Flow.js
+++ Soft-Uni-JS-Programming-Basics/05-For-Loops/05-Lab-Exercises/Symbols Flow.js
@@ -2,3 +2,13 @@
 
 Напишете програма, която чете текст(стринг), въведен от потребителя и печата всеки символ от текста на отделен ред.*/
 
+function flow(input) {
+  let text = input.shift();
+
+  for (i = 0; i < text.length; i++) {
+    let letter = text[i]; //zashto i e v array?
+    console.log(letter);
+  }
+}
+
+flow(["softuni"]);
